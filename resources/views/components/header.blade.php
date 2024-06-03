db16483542081345190eaca48086a0ebf372c0cf
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2024-06-03 22:55:47 +0600

    add: active route

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,32 @@
+  @php
+      $navItems = [
+          ['url' => '/', 'label' => 'Home'],
+          ['url' => '/about', 'label' => 'About'],
+          ['url' => '/blog', 'label' => 'Blog'],
+          ['url' => '/contact', 'label' => 'Contact'],
+      ];
+  @endphp
+
+  <header class="navbar-light bg-light">
+      <div class="container">
+          <nav class="navbar navbar-expand-lg ">
+              <div class="container-fluid">
+                  <a class="navbar-brand" href="{{ url('/') }}">My Application</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarNav">
+                      <ul class="navbar-nav">
+                          @foreach ($navItems as $item)
+                              <li class="nav-item">
+                                  <a class="nav-link {{ request()->is(ltrim($item['url'], '/')) ? 'text-primary' : '' }}"
+                                      href="{{ url($item['url']) }}">{{ $item['label'] }}</a>
+                              </li>
+                          @endforeach
+                      </ul>
+                  </div>
+              </div>
+          </nav>
+      </div>
+  </header>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,35 +10,8 @@
 </head>
 
 <body>
-    <header class="navbar-light bg-light">
-        <div class="container">
-            <nav class="navbar navbar-expand-lg ">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="{{ url('/') }}">My Application</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav ">
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ url('/') }}">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ url('/about') }}">About</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ url('/blog') }}">Blog</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ url('/contact') }}">Contact</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    </header>
+
+    @include('components.header')
     <main>
         <div class="container">
             @yield('content')
