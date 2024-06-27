f4433dfad35b70b79d8d6557036577df73857771
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2024-06-28 00:04:45 +0600

    add: create job

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -8,6 +8,8 @@
     <div>Name: {{ $job['title'] }}</div>
     <div>Company: ${{ $job['salary'] }}</div>
 
-
+    <div class="mt-4">
+        <a href="{{ route('jobs.edit', $job['id']) }}" class="btn btn-danger">Edit Job</a>
+    </div>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,24 @@ Route::get('/', function () {
 
     return view('home');
 });
+
+// index
 Route::get('/jobs', function () {
     return view('jobs.index', [
         'jobs' => Job::all()
     ]);
 });
 
+//create 
 Route::get("/jobs/create", function () {
     return view('jobs.create');
 });
+//store
 Route::post("/jobs", function () {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
     Job::create([
         'title' => request('title'),
         'salary' => request('salary'),
@@ -25,6 +33,7 @@ Route::post("/jobs", function () {
     return redirect('/jobs');
 });
 
+//show
 Route::get('/job/{id}', function ($id) {
     $job = Job::find($id);
 
@@ -34,6 +43,15 @@ Route::get('/job/{id}', function ($id) {
         abort(404, 'Job not found');
     }
 });
+
+//edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+    return view('jobs.edit', ['job' => $job]);
+});
+
+
+
 
 Route::get('/about', function () {
     return view('about');
